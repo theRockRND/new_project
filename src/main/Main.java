3fc4bd85aad47ed0a9b5043631dd4846a05262e0
theRockRND <myu.trubitsin@mail.ru>
2022-02-02 20:34:07 +0300

    create project

---
+++ src/main/Main.java
@@ -0,0 +1,10 @@
+package main;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+      assertEquals (4,2+2);
+    }
+}
